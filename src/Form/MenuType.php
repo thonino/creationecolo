8aa7f2c4df8fede8ce3f7e85408003b5d60372d8
thonino <thoninoben@gmail.com>
2022-03-23 20:11:04 +0100

    MAJ category Menu Products BOOTSTRAP

--- src/Form/MenuType.php
+++ src/Form/MenuType.php
@@ -6,13 +6,14 @@ use App\Entity\Menu;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class MenuType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Nom'])
         ;
     }
 
--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -6,15 +6,16 @@ use App\Entity\Products;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProductsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('image')
-            ->add('description')
+            ->add('name',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Nom'])
+            ->add('image',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Image'])
+            ->add('description',TextType::class,[ 'attr' => ['class' => 'form-control'], 'label' => 'Description'])
             ->add('category')
         ;
     }
