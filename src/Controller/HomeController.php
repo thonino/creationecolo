b9f0b1897a2e652ef5fdb509a9c3931ffda6718f
thonino <thoninoben@gmail.com>
2022-03-27 19:30:58 +0200

    maj home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,18 +1,19 @@
 <?php
 
 namespace App\Controller;
+
 use App\Repository\MenuRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(MenuRepository $menuRepository): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('/home/index.html.twig', [
             'menus' => $menuRepository->findAll(),
         ]);
-    }
+}
 }
--- src/Controller/PartialController.php
+++ src/Controller/PartialController.php
@@ -1,20 +1,19 @@
 <?php
 
 namespace App\Controller;
-use App\Entity\Menu;
 use App\Repository\MenuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/Partial')]
 class PartialController extends AbstractController
 {
     #[Route('/nav', name: 'app_partial_nav')]
     public function nav(MenuRepository $menuRepository): Response
     {
         return $this->render('partial/nav.html.twig', [
-            'controller_name' => 'PartialController',
-            'menus' => $menuRepository->findAll(),
+            // 'menus' => $menuRepository->findAll(),
         ]);
     }
     #[Route('/footer', name: 'app_partial_footer')]
