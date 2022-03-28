7a6ebe6fe0957a475305e2e7c786d0b620b91c9d
thonino <thoninoben@gmail.com>
2022-03-28 23:14:42 +0200

    nav ctrl

---
+++ src/Controller/NavController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class NavController extends AbstractController
+{
+    #[Route('/nav', name: 'app_nav')]
+    public function index(): Response
+    {
+        return $this->render('nav/index.html.twig', [
+            'controller_name' => 'NavController',
+        ]);
+    }
+}
