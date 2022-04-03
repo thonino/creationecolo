022550a249621e7c2f41750ab99bea9774f31d0d
thonino <thoninoben@gmail.com>
2022-04-03 16:03:07 +0200

    panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,11 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-#[Route(['en' => '/cart','fr' => '/panier',])]
+#[Route('/cart')]
 class CartController extends AbstractController
 {
     // Fonction ADD
-    #[Route(['en' => '/{products}/add','fr' => '/{products}/ajouter',], name: 'cart_add')]
+    #[Route(['/{products}/ajouter'], name: 'cart_add')]
     public function add(Products $products, SessionInterface $session): Response
     {
         $cart = $session->get('cart', []);
@@ -21,7 +21,7 @@ class CartController extends AbstractController
         return $this->redirectToRoute('cart_show', [ 'id'=>$id,]);
     }
     // Fonction LESS
-    #[Route(['en' => '/{products}/less','fr' => '/{products}/moins',], name: 'cart_less')]
+    #[Route(['/{products}/moins'], name: 'cart_less')]
     public function less(Products $products, SessionInterface $session): Response
     {
         $cart = $session->get('cart', []);
@@ -32,7 +32,7 @@ class CartController extends AbstractController
         return $this->redirectToRoute('cart_show', [ 'id'=>$id,]);
     }
     // Fonction supprimer
-    #[Route(['en' => '/{products}/del','fr' => '/{products}/suprimmer',], name: 'cart_del')]
+    #[Route(['/{products}/suprimmer'], name: 'cart_del')]
     public function remove(Products $products, SessionInterface $session): Response
     {
         $cart = $session->get('cart', []);
@@ -41,20 +41,20 @@ class CartController extends AbstractController
         $session->set('cart', $cart);
         return $this->redirectToRoute('cart_show', [ 'id'=>$id,]);
     }
-    #[Route(['en' => '/show','fr' => '/voir',], name: 'cart_show')]
+    #[Route(['/show'], name: 'cart_show')]
     public function show(SessionInterface $session, ProductsRepository $productsRepo): Response
     {
-        $fullCart = [];
         $total = 0;
+        $fullCart = [];
         $cart = $session->get('cart', []);
-        foreach($cart as $id =>$qty){
+        foreach($cart as $id =>$quantite){
             $products = $productsRepo->find($id);  
-            $fullCart[]= ['products' => $products,'qty' => $qty,];
-            $total += $products->getPrice()*$qty;
+            $fullCart[]= ['products' => $products,'quantite' => $quantite,];
+            $total += $products->getPrice() * $quantite;
         }
         return $this->render('cart/cart.html.twig', [
             'cartProducts'=>$fullCart,
-            'total' =>$total,
+            'total'=>$total,
         ]);
     }
 }
